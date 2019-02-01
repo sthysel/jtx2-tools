f624f22ff9496600f56ec45277ff3449eb7d5a77
Thys Meintjes <sthysel@gmail.com>
2019-02-01 15:00:15 +0800

    fix annoying global state

--- src/jtx2tools/cli.py
+++ src/jtx2tools/cli.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plot
 from matplotlib.animation import FuncAnimation
 from collections import deque
 
-# matplotlib.use('TkAgg')
+BHP_COLOUR = (0, 84, 230)  # BHP orange
+
 
 @click.command(context_settings=dict(max_content_width=120))
 @click.version_option()
@@ -22,6 +23,7 @@ from collections import deque
     '-f',
     '--gpu-load-file',
     default='/sys/devices/gpu.0/load',
+    #default='/sys/class/thermal/thermal_zone1/temp',
     help='Load file',
     show_default=True,
 )
@@ -30,10 +32,15 @@ def cli(interval, gpu_load_file):
     Plot JTX2 GPU usage
     """
 
-    #global gpu_ax, gpu_line, gpuy_list, gpu_xlist, fill_lines
+    fig = plot.figure(figsize=(6, 2))
+
+    fig.set_facecolor('#F2F1F0')
+    fig.canvas.set_window_title('JTX2 GPU Activity')
 
     # Subplot for the GPU activity
     gpu_ax = plot.subplot2grid((1, 1), (0, 0), rowspan=2, colspan=1)
+    plot.subplots_adjust(top=0.85, bottom=0.30)
+
     # For the comparison
     gpu_line, = gpu_ax.plot([], [])
     # The line points in x,y list form
@@ -42,7 +49,9 @@ def cli(interval, gpu_load_file):
 
     fill_lines = []
 
-    def initGraph():
+    def init_graph():
+        """initial empty frame"""
+
         gpu_ax.set_xlim(60, 0)
         gpu_ax.set_ylim(-5, 105)
         gpu_ax.set_title('GPU History')
@@ -55,43 +64,36 @@ def cli(interval, gpu_load_file):
 
         return [gpu_line] + [fill_lines]
 
-    def updateGraph(frame):
+    def update_graph(frame):
         """Now draw the GPU usage"""
 
         gpuy_list.popleft()
         try:
-            with open(gpu_load_file, 'r') as gpuFile:
-                fileData = gpuFile.read()
-
+            with open(gpu_load_file, 'r') as f:
+                reading = f.read()
                 # The GPU load is stored as a percentage * 10, e.g 256 = 25.6%
-                gpuy_list.append(int(fileData) / 10)
+                gpuy_list.append(int(reading) / 10)
                 gpu_line.set_data(gpux_list, gpuy_list)
-                #fill_lines.remove()
                 fill_lines = gpu_ax.fill_between(
                     gpux_list,
                     0,
                     gpuy_list,
-                    facecolor='cyan',
+                    facecolor='orange',
                     alpha=0.50,
                 )
-
                 return [gpu_line] + [fill_lines]
+
         except FileNotFoundError:
             click.echo(f'Error reading {gpu_load_file}')
             return []
 
-    fig = plot.figure(figsize=(6, 2))
     animation = FuncAnimation(
-        fig,
-        updateGraph,
+        fig=fig,
+        func=update_graph,
         frames=200,
-        init_func=initGraph,
+        init_func=init_graph,
         interval=interval,
         blit=True,
     )
-
-    plot.subplots_adjust(top=0.85, bottom=0.30)
-    fig.set_facecolor('#F2F1F0')
-    fig.canvas.set_window_title('GPU Activity Monitor')
 
     plot.show()
